96e59fe593e4cdd7e5d4c16e9cd6a227faa44b3a
Nazeefa-shahzad <143958372+Nazeefa-shahzad@users.noreply.github.com>
2023-09-03 23:45:49 +0500

    Add files via upload

---
+++ Assignment1part2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+# Calc35ulate gross pay
+gross_pay = hours * rate
+
+# Display the result
+print("Pay:", gross_pay)
+
+
+# In[ ]:
+
+
+
+
